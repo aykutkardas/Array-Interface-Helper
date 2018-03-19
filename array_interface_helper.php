641335b244a97059930cc0ab33252de4b59da5de
aykutkardas <aykutkrds@gmail.com>
2018-03-19 22:59:20 +0300

    Initial Commit

---
+++ array_interface_helper.php
@@ -0,0 +1,151 @@
+<?php
+/*
+ * Array Interface Helper
+ * Author: Aykut Kardaş
+ * Github: http://github.com/aykutkardas
+ * Email:  aykutkrds@gmail.com
+ */
+
+
+if(!function_exists('array_interface')) {
+
+  function array_interface($interface, $array, $callback)
+  {
+
+    // Create Report.
+    $report = array();
+    // Create Controlled Array.
+    $controlledArray = array();
+    // Error Switch.
+    $error = false;
+
+    foreach($interface as $key => $value) {
+
+      // Rule type checking
+      if(gettype($interface[$key]) == 'array') {
+
+        // Interface Property Rules
+        foreach($interface[$key] as $k => $v) {
+          
+          
+          // Required control
+          if($k === 'required') {
+            if(!ai_check_required($v, $array[$key])) {
+              $report[$key][$k] = "This field can not be left blank!";
+              $error = true;
+            }
+          }
+          
+          // Property Type Control
+          if($k === 'type') {
+            if(!ai_check_type($v, $array[$key])){
+              $report[$key][$k] = "This field can only be '$v!";
+              $error = true;
+            };
+          }
+          
+          // Propery Max Length Control
+          if($k === 'max_length') {
+            if(!ai_check_max_length($v, $array[$key])){
+                $report[$key][$k] = "This field can be up to '$v' lengths long!";
+                $error = true;
+            };
+          }
+          
+          // Propery Min Length 
+          if($k === 'min_length') {
+            if(!ai_check_min_length($v, $array[$key])){
+                $report[$key][$k] = "This field can be at least '$v' long!";
+                $error = true;
+            };
+          }
+
+          $controlledArray[$key] = $array[$key];
+
+          
+
+        }
+
+      } else {
+        if(!ai_check_required($interface[$key], $array[$key])) {
+          $report[$key]['required'] = "This field can not be left blank!";
+          $error = true;
+        }
+
+        if(isset($array[$key]))
+          $controlledArray[$key] = $array[$key];
+      }
+
+    }
+
+    return array(
+      'error' => $error ? $report : false,
+      'data'  => $controlledArray
+    );
+
+  }
+
+
+  // Type check.
+  // $type = "string|integer|boolean|function|array"
+  function ai_check_type($type, $data)
+  {
+    if(!isset($data)) return true;
+    
+    if($type === gettype($data))
+      return true;
+    
+    return false;
+  }
+
+  // Check max length.
+  // $len = integer
+  function ai_check_max_length($len, $data)
+  {
+    if(!isset($data)) return true;
+
+    if(gettype($data) === 'array')
+      $data_length = count($data);
+    else if(gettype($data) === 'string')
+      $data_length = strlen($data);
+    else
+      return false;
+
+    if($data_length < $len)
+      return true;
+
+    return false;
+  }
+
+  // Check min length.
+  // $len = integer
+  function ai_check_min_length($len, $data)
+  {
+    if(!isset($data)) return true;
+
+    if(gettype($data) === 'array')
+      $data_length = count($data);
+    else if(gettype($data) === 'string')
+      $data_length = strlen($data);
+    else
+      return false;
+
+    if($data_length > $len)
+      return true;
+
+    return false;
+  }
+
+  // Check required
+  // $required = true|false
+  function ai_check_required($required, $data)
+  {
+    if($required === true && isset($data))
+      return true;
+    else if($required === false)
+      return true;
+
+    return false;
+  }
+
+}
